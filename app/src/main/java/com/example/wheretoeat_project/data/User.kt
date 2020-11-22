19cf101f18fc599d548bc82c58403a93e04605e3
akostv23 <tordai.akos@student.ms.sapientia.ro>
2020-11-22 17:07:28 +0200

    ROOM implementation - update

--- app/src/main/java/com/example/wheretoeat_project/data/User.kt
+++ app/src/main/java/com/example/wheretoeat_project/data/User.kt
@@ -1,4 +1,14 @@
 package com.example.wheretoeat_project.data
 
-class User {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val firstName: String,
+    val lastName: String,
+    val age: Int
+)
+
